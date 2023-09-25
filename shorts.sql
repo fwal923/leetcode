96ffb11a569ad376f6a7cf94c0dfaf75ae462964
Fiona Walter <73806661+fwal923@users.noreply.github.com>
2023-09-25 15:10:39 +0200

    Update shorts.sql

--- shorts.sql
+++ shorts.sql
@@ -1,3 +1,10 @@
+-- 595. Big Countries
+select
+name, population, area
+from
+world
+where population >= 25000000 or area >= 3000000
+
 -- 1527. Patients With a Condition
 select
 patient_id, patient_name, conditions
