04b2527a812ab9f776d2212b9be7a1522526392b
Fiona Walter <73806661+fwal923@users.noreply.github.com>
2023-09-25 15:24:52 +0200

    Update shorts.sql

--- shorts.sql
+++ shorts.sql
@@ -1,3 +1,5 @@
+-- MS SQL Server
+
 -- 595. Big Countries
 select
 name, population, area
@@ -11,3 +13,10 @@ patient_id, patient_name, conditions
 from
 patients
 where conditions like '% DIAB1%' or conditions like 'DIAB1%'
+
+-- 1581. Customer Who Visited but Did Not Make Any Transactions
+select customer_id, count (v.visit_id) as count_no_trans
+from visits v
+left outer join transactions t on v.visit_id = t.visit_id
+where t.visit_id IS NULL
+group by customer_id
